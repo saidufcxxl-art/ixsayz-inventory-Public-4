3a750e9912e5f87b7db3bce03efe1ddd26e52b15
saidufcxxl-art <saidufcxxl@gmail.com>
2026-07-02 14:54:25 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+﻿const CACHE='ixsayz-clean-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./assets/icons/icon-192.png','./assets/icons/icon-512.png'])));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
